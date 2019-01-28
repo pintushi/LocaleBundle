38c3271e0ce167ef7c35f1e2c91317b7299cce13
Vidy Videni <vidy.videni@gmail.com>
2019-01-28 14:29:57 +0800

    remove unused classes

--- src/Pintushi/Bundle/CoreBundle/DependencyInjection/Compiler/AppPass.php
+++ src/Pintushi/Bundle/CoreBundle/DependencyInjection/Compiler/AppPass.php
@@ -6,14 +6,6 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
-use Pintushi\Bundle\CoreBundle\Metadata\CachedPropertyNameCollectionFactory;
-use Pintushi\Bundle\CoreBundle\ORM\Extension\PaginationExtension;
-use Pintushi\Bundle\CoreBundle\ORM\CollectionDataProvider;
-use Pintushi\Bundle\CoreBundle\Serializer\Hal\ApiRelationItemNormalizer;
-use Pintushi\Bundle\CoreBundle\Serializer\Hal\ApiRelationCollectionNormalizer;
-use Pintushi\Bundle\CoreBundle\EventListener\DeserializeListener;
-use Pintushi\Bundle\CoreBundle\EventListener\WriteListener;
-
 class AppPass implements CompilerPassInterface
 {
     /**
@@ -21,7 +13,6 @@ class AppPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-
         $container->getDefinition('sm.callback.cascade_transition')->setPublic(true);
     }
 }
